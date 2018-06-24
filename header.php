26a56096d5c1e917e78ca7f48d555cb9d119ae6c
Flyzy2005 <flywf@whu.edu.cn>
2018-06-24 21:38:38 +0800

    do some fix

--- header.php
+++ header.php
@@ -7,63 +7,44 @@
  */
 ?><!DOCTYPE HTML>
 <html lang="zh-hans">
-<head>
-    <title><?php wp_title( '-', true, 'right' ); ?></title>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <meta name="keywords" content="<?php kratos_keywords(); ?>"/>
-	<?php wp_head(); ?>
-	<?php wp_print_scripts( 'jquery' ); ?>
-	<?php if ( kratos_option( 'site_bw' ) == 1 ) : ?>
-        <style type="text/css">html {
-                filter: grayscale(100%);
-                -webkit-filter: grayscale(100%);
-                -moz-filter: grayscale(100%);
-                -ms-filter: grayscale(100%);
-                -o-filter: grayscale(100%);
-                filter: progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);
-                filter: gray;
-                -webkit-filter: grayscale(1);
-            }
-        </style>
-	<?php endif; ?>
-	<?php if ( is_singular() ) {
-		wp_enqueue_script( 'comment-reply' );
-	} ?>
-</head>
-<?php flush(); ?>
-<body data-spy="scroll" data-target=".scrollspy">
-<div id="kratos-wrapper">
-    <div id="kratos-page">
-        <div id="kratos-header">
-            <header id="kratos-header-section">
-                <div class="header-container">
-                    <div class="container">
-                        <div class="nav-header">
-							<?php if ( has_nav_menu( 'header_menu' ) ) : ?>
-                                <a class="js-kratos-nav-toggle kratos-nav-toggle" style="margin: 0; line-height: 48px"><i></i></a>
-							<?php endif; ?>
-							<?php $site_logo = kratos_option( 'site_logo' ); ?>
-							<?php if ( ! empty( $site_logo ) ) { ?>
-                                <a href="<?php echo get_option( 'home' ); ?>">
-                                    <h1 id="kratos-logo-img"><img src="<?php echo $site_logo; ?>"></h1>
-                                </a>
-							<?php } else { ?>
-                                <h2 id="kratos-logo"><a
-                                            href="<?php echo get_option( 'home' ); ?>"><?php bloginfo( 'name' ); ?></a>
-                                </h2>
-							<?php } ?>
-							<?php $defaults = array(
-								'theme_location' => 'header_menu',
-								'container'      => 'nav',
-								'container_id'   => 'kratos-menu-wrap',
-								'menu_class'     => 'sf-menu',
-								'menu_id'        => 'kratos-primary-menu',
-							); ?>
-							<?php wp_nav_menu( $defaults ); ?>
-                        </div>
-                    </div>
-                </div>
-            </header>
-        </div>
+	<head>
+		<title><?php wp_title( '-', true, 'right' ); ?></title>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta name="keywords" content="<?php kratos_keywords();?>" />
+		<?php wp_head(); ?>
+		<?php wp_print_scripts('jquery'); ?>
+		<?php if ( kratos_option('site_bw')==1 ) : ?>
+			<style type="text/css">html{filter: grayscale(100%);-webkit-filter: grayscale(100%);-moz-filter: grayscale(100%);-ms-filter: grayscale(100%);-o-filter: grayscale(100%);filter: progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);filter: gray;-webkit-filter: grayscale(1); }
+			</style>
+		<?php endif; ?>
+		<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' );?>
+	</head>
+	<?php flush(); ?>
+	<body data-spy="scroll" data-target=".scrollspy">
+		<div id="kratos-wrapper">
+			<div id="kratos-page">
+				<div id="kratos-header">
+					<header id="kratos-header-section">
+						<div class="header-container">
+						<div class="container" style="padding-right:5px">
+							<div class="nav-header">
+								<?php if ( has_nav_menu('header_menu') ) :?>
+									<a class="js-kratos-nav-toggle kratos-nav-toggle" style="margin: 0; line-height: 48px"><i></i></a>
+								<?php endif; ?>
+								<?php $site_logo = kratos_option('site_logo');?>
+								<?php if ( !empty( $site_logo ) ) {?>
+									<a href="<?php echo get_option('home'); ?>">
+									<h1 id="kratos-logo-img"><img src="<?php echo $site_logo; ?>"></h1>
+									</a>
+								<?php }else{?>
+									<h2 id="kratos-logo"><a href="<?php echo get_option('home'); ?>"><?php bloginfo('name'); ?></a></h2>
+								<?php }?>
+								<?php $defaults = array('theme_location' => 'header_menu', 'container' => 'nav', 'container_id' => 'kratos-menu-wrap', 'menu_class' => 'sf-menu', 'menu_id' => 'kratos-primary-menu', ); ?>
+							 <?php wp_nav_menu($defaults); ?>
+							</div>
+						</div>
+						</div>
+					</header>
+				</div>
--- index.php
+++ index.php
@@ -6,27 +6,23 @@
  * @version 2.3
  */
 
-get_header(); ?>
-<?php
-
-if ( is_category() ) {
-	get_header( 'abstract' );
-} else {
-	get_header( 'banner' );
-} ?>
+get_header();
+get_header( 'banner' ); ?>
 <div id="kratos-blog-post" style="background:<?php echo kratos_option( 'background_index_color' ); ?>">
     <div class="container">
         <div class="row">
             <section id="main" class="col-md-8">
 				<?php
 				if ( is_home() ) {
-					kratos_banner();
+					if ( kratos_option( 'top_post' ) == 1 ) :
+						kratos_banner();
+					endif;
 				} elseif ( is_category() ) {
-					kratos_banner();
-					global $query_string;
-					query_posts( $query_string . "&order=ASC" );
+					if ( kratos_option( 'top_post' ) == 1 ) :
+						kratos_banner();
+					endif;
 					?>
-					<?php if ( kratos_option( 'show_head_cat' ) ): ?>
+					<?php if ( kratos_option( 'show_head_cat' ) ) : ?>
                         <div class="kratos-hentry clearfix">
                             <h1 class="kratos-post-header-title">分类：<?php echo single_cat_title( '', false ); ?></h1>
                             <h1 class="kratos-post-header-title"><?php echo category_description(); ?></h1>
@@ -34,11 +30,11 @@ if ( is_category() ) {
 					<?php endif; ?>
 					<?php
 				} elseif ( is_tag() ) {
-					kratos_banner();
-					global $query_string;
-					query_posts( $query_string . "&order=ASC" );
+					if ( kratos_option( 'top_post' ) == 1 ) :
+						kratos_banner();
+					endif;
 					?>
-					<?php if ( kratos_option( 'show_head_tag' ) ): ?>
+					<?php if ( kratos_option( 'show_head_tag' ) ) : ?>
                         <div class="kratos-hentry clearfix">
                             <h1 class="kratos-post-header-title">标签：<?php echo single_cat_title( '', false ); ?></h1>
                             <h1 class="kratos-post-header-title"><?php echo category_description(); ?></h1>
@@ -46,9 +42,9 @@ if ( is_category() ) {
 					<?php endif; ?>
 					<?php
 				} elseif ( is_search() ) {
-					kratos_banner();
-					global $query_string;
-					query_posts( $query_string . "&order=ASC" );
+					if ( kratos_option( 'top_post' ) == 1 ) :
+						kratos_banner();
+					endif;
 					?>
                     <div class="kratos-hentry clearfix">
                         <h1 class="kratos-post-header-title">搜索结果：<?php the_search_query(); ?></h1>
@@ -73,7 +69,7 @@ if ( is_category() ) {
 				<?php wp_reset_query(); ?>
             </section>
             <aside id="kratos-widget-area" class="col-md-4 hidden-xs hidden-sm scrollspy">
-					<?php dynamic_sidebar( 'sidebar_tool' ); ?>
+				<?php dynamic_sidebar( 'sidebar_tool' ); ?>
             </aside>
         </div>
     </div>
